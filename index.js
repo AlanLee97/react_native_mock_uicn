1fed4b25555ac01649e24b55b83f4f662e70319b
AlanLee <1445654576@qq.com>
2020-08-05 17:25:01 +0800

    添加获取屏幕宽高的函数

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Dimensions} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import React from 'react';
@@ -10,6 +10,22 @@ import React from 'react';
 React.mockPath = 'http://localhost:8081/src/assets/mock/';
 React.createDefaultValue = (value, defaultValue) => {
   return value === undefined ? defaultValue : value;
+}
+
+React.getScreenInfo = () => {
+  let screenWidth = Dimensions.get('window').width;
+  let screenHeight = Dimensions.get('window').height;
+  return {screenWidth, screenHeight};
+}
+
+React.getScreenWidth = () => {
+  let screenWidth = Dimensions.get('window').width;
+  return screenWidth;
+}
+
+React.getScreenHeight = () => {
+  let screenHeight = Dimensions.get('window').height;
+  return screenHeight;
 }
 
 AppRegistry.registerComponent(appName, () => App);

c6ed765cc8a8ff048c2346caaf4fa89bb83a0b84
AlanLee <1445654576@qq.com>
2020-08-05 17:01:39 +0800

    完成主页的大致样式

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
-import {View, Text, Button, StatusBar} from 'react-native'
+import {View, Text, StatusBar, ScrollView} from 'react-native'
+import styles from '../../assets/style/Styles';
+import ShowWorkBox from './component/ShowWorkBox';
 
 class HomePage extends React.Component{
 
@@ -8,23 +10,104 @@ class HomePage extends React.Component{
     super(props);
     this.state = {
       navigation: props.navigation,
+      workList: [
+        {
+          poster: require('../../assets/image/my/poster1.png'),
+          title: "UI中国手机客户端原创设计",
+          look: 23421,
+          like: 420,
+          comment: 4232,
+          tag: ['原创'],
+          user: {
+            username: "AlanLee",
+            avatar: "https://gitee.com/AlanLee97/assert/raw/master/note_images/naruto.jpg",
+            identity: "UI设计师",
+          }
+        },
+        {
+          poster: require('../../assets/image/my/poster2.png'),
+          title: "「汉学」文学工具产品的视觉与体验碰撞",
+          look: 23421,
+          like: 420,
+          comment: 4232,
+          tag: ['原创'],
+          user: {
+            username: "AlanLee",
+            avatar: "https://gitee.com/AlanLee97/assert/raw/master/note_images/naruto.jpg",
+            identity: "UI设计师",
+          }
+        },
+        {
+          poster: require('../../assets/image/my/poster3.png'),
+          title: "【顷刻】_视听解说APP",
+          look: 23421,
+          like: 420,
+          comment: 4232,
+          tag: ['原创'],
+          user: {
+            username: "AlanLee",
+            avatar: "https://gitee.com/AlanLee97/assert/raw/master/note_images/naruto.jpg",
+            identity: "UI设计师",
+          }
+        },
+        {
+          poster: require('../../assets/image/my/poster4.png'),
+          title: "拼多多APP REDESIGN",
+          look: 23421,
+          like: 420,
+          comment: 4232,
+          tag: ['原创'],
+          user: {
+            username: "AlanLee",
+            avatar: "https://gitee.com/AlanLee97/assert/raw/master/note_images/naruto.jpg",
+            identity: "UI设计师",
+          }
+        },
+        {
+          poster: require('../../assets/image/my/poster5.png'),
+          title: "Redesign《在外》APP ",
+          look: 23421,
+          like: 420,
+          comment: 4232,
+          tag: ['原创'],
+          user: {
+            username: "AlanLee",
+            avatar: "https://gitee.com/AlanLee97/assert/raw/master/note_images/naruto.jpg",
+            identity: "UI设计师",
+          }
+        }
+      ]
     };
   }
 
   // 渲染函数
   render() {
-    const navigation = this.state.navigation;
+    const state = this.state;
     return (
-        <View style={{flex: 1,
-          justifyContent: 'center', alignItems: 'center'}}>
-          <Text>HomePage</Text>
-
+        <View style={{backgroundColor: "#fff",
+        paddingTop: 30}}>
           {/*设置状态栏（透明）*/}
           <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0)" barStyle={'dark-content'} />
-          {/*<Text>接收的参数：{uid}</Text>*/}
-          <Button title="去登录" onPress={() => {navigation.push('LoginPage')}}></Button>
-          <Text></Text>
-          <Button title="去测试页面" onPress={() => {navigation.push('TestNetwork')}}></Button>
+
+          {/*标题栏*/}
+          <View style={[styles.alFlexRow, styles.alPaddingLR20, styles.alPaddingTB10]}>
+            <Text style={[styles.alColorBlue, styles.alTextH4]}>首推</Text>
+            <Text style={[styles.alColorGray, styles.alTextH4, styles.alMarginLeft20]}>关注</Text>
+            <Text style={[styles.alColorGray, styles.alTextH4, styles.alMarginLeft20]}>最新</Text>
+            <Text style={[styles.alColorGray, styles.alTextH4, styles.alMarginLeft20]}>佳作分享</Text>
+          </View>
+
+          {/*作品列表*/}
+          <ScrollView>
+            {
+              state.workList.map((item, index) => {
+                return <ShowWorkBox data={item} />
+              })
+            }
+
+            <View style={{height: 50}}></View>
+          </ScrollView>
+
         </View>
     );
   }
---
+++ src/pages/home/component/ShowWorkBox.js
@@ -0,0 +1,67 @@
+import React from "react";
+import {View, Text} from 'react-native'
+import styles from '../../../assets/style/Styles';
+import AvatarAndNickname from '../../my/component/AvatarAndNickname';
+import ALImage from '../../../components/al-image/ALImage';
+import ALTag from '../../../components/al-tag/ALTag';
+
+class ShowWorkBox extends React.Component{
+
+  //构造器
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // 渲染函数
+  render() {
+    const props = this.props;
+    return <View key={props.data.title} style={[styles.alMarginLR20, styles.alMarginBottom30]}>
+      <AvatarAndNickname userInfo={props.data.user} />
+      <View style={{height: 10}}></View>
+      <ALImage src={props.data.poster} width={React.getScreenWidth() - 40} height={React.getScreenWidth() - 40} radius={20} />
+      <Text numberOfLines={1} style={[styles.alTextH4, styles.alMarginTB10]}>{props.data.title}</Text>
+      <View style={[styles.alFlexRow, styles.alFlexSpaceBetween]}>
+        {/*显示标签*/}
+        <View>
+          {
+            props.data.tag.map((item2, index2) => {
+              return <ALTag key={item2} text={item2} />
+            })
+          }
+        </View>
+        <View style={[styles.alFlexRow]}>
+          <View style={[styles.alFlexRow]}>
+            <ALImage src={require('../../../assets/icon/home/look.png')} width={20} height={20} />
+            <Text style={{color: '#cdcdcd'}}>{props.data.look}    </Text>
+          </View>
+          <View style={[styles.alFlexRow]}>
+            <ALImage src={require('../../../assets/icon/home/like.png')} width={18} height={18} />
+            <Text style={{color: '#cdcdcd'}}>{props.data.like}    </Text>
+          </View>
+          <View style={[styles.alFlexRow]}>
+            <ALImage src={require('../../../assets/icon/home/comment.png')} width={20} height={20} />
+            <Text style={{color: '#cdcdcd'}}>{props.data.comment}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  }
+
+  // 生命周期函数
+  //组件已挂载
+  componentDidMount() {
+
+  }
+
+  //组件将要卸载时
+  componentWillUnmount() {
+
+  }
+
+
+}
+
+export default ShowWorkBox;

eb843e854b29e1ba87e10a477f960044e41f5f62
AlanLee <1445654576@qq.com>
2020-08-07 00:19:04 +0800

    加入插槽功能

--- src/components/al-divider/ALDivider.js
+++ src/components/al-divider/ALDivider.js
@@ -1,29 +1,54 @@
-import React from "react";
-import {View, Text, Dimensions} from 'react-native'
+import React from 'react';
+import {View, Text, Dimensions} from 'react-native';
+import styles from '../../assets/style/Styles';
 
 let screenWidth = Dimensions.get('window').width;
 let screenHeight = Dimensions.get('window').height;
 
-class ALDivider extends React.Component{
+class ALDivider extends React.Component {
 
   //构造器
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
 
   // 渲染函数
   render() {
-    const props = this.props;
     return (
-        <View style={{
-          margin: React.createDefaultValue(props.margin, 0),
-          width: React.createDefaultValue(props.width, screenWidth),
-          height: React.createDefaultValue(props.weight, 0.5),
-          backgroundColor: React.createDefaultValue(props.color, '#eeeeee')
-        }} />
+        <View>
+          {
+            this.props.children ? this.props.children :
+                <View>
+                  {
+                    this.props.slotCenter ?
+                        <View style={[styles.alFlexRow, styles.alFlexCenter]}>
+                          <View style={{
+                            flex: 1,
+                            flexDirection: 'column',
+                            height: React.createDefaultValue(this.props.weight, 0.5),
+                            backgroundColor: React.createDefaultValue(this.props.color, '#eee'),
+                          }}/>
+                          <View style={{flex: 1}}>
+                            {this.props.slotCenter}
+                          </View>
+                          <View style={{
+                            flex: 1,
+                            height: React.createDefaultValue(this.props.weight, 0.5),
+                            backgroundColor: React.createDefaultValue(this.props.color, '#eee'),
+                          }}/>
+                        </View> :
+                        <View style={{
+                          margin: React.createDefaultValue(this.props.margin, 0),
+                          width: React.createDefaultValue(this.props.width, screenWidth),
+                          height: React.createDefaultValue(this.props.weight, 0.5),
+                          backgroundColor: React.createDefaultValue(this.props.color, '#eeeeee'),
+                        }}/>
+                  }
+                </View>
+          }
+        </View>
+
     );
   }
 
@@ -37,7 +62,6 @@ class ALDivider extends React.Component{
   componentWillUnmount() {
 
   }
-
 
 
 }

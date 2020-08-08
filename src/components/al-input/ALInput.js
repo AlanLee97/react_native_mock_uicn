218eab3c66241cee0004bd0beb02cc48dd1cc864
1445654576@qq.com <1445654576@qq.com>
2020-08-08 14:03:40 +0800

    更改底部边框样式

--- src/components/al-input/ALInput.js
+++ src/components/al-input/ALInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, TextInput} from 'react-native';
 import ALDivider from '../al-divider/ALDivider';
+import styles from '../../assets/style/Styles';
 
 class ALInput extends React.Component {
 
@@ -20,24 +21,31 @@ class ALInput extends React.Component {
     const screenHeight = React.getScreenHeight();
 
     return (
-        <View>
-          <TextInput {...this.props}
-                     onFocus={(e) => {
-                       this.setState({
-                         focused: true,
-                       });
-                     }}
-                     onBlur={() => {
-                       this.setState({
-                         focused: false,
-                       });
-                     }}
-                     secureTextEntry={this.props.type === 'password'}
-                     selectionColor={React.createDefaultValue(this.props.primaryColor,'#3e9bf7')}
-          />
-          <ALDivider width={screenWidth - 60}
-                     weight={state.focused ? 2 : 0.5}
-                     color={state.focused ? React.createDefaultValue(this.props.primaryColor, '#3e9bf7') : '#eee'}/>
+        <View style={{flex: 1}}>
+          <View>
+            <TextInput {...this.props}
+                       onFocus={(e) => {
+                         this.setState({
+                           focused: true,
+                         });
+                       }}
+                       onBlur={() => {
+                         this.setState({
+                           focused: false,
+                         });
+                       }}
+                       secureTextEntry={this.props.type === 'password'}
+                       selectionColor={React.createDefaultValue(this.props.primaryColor, styles.alColorBlue.color) }
+            />
+          </View>
+          <View style={{flex: 1}}>
+            <ALDivider
+              weight={state.focused ? 2 : 0.5}
+              color={state.focused ?
+                React.createDefaultValue(this.props.primaryColor, styles.alColorBlue.color)
+                :
+                styles.alColorGray.color}/>
+          </View>
         </View>
     );
   }

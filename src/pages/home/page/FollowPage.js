5a29affea5732003377114d8c32bed4d2ee38136
1445654576@qq.com <1445654576@qq.com>
2020-08-16 09:48:58 +0800

    添加页面

---
+++ src/pages/home/page/FollowPage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {View, Text} from 'react-native'
+
+class FollowPage extends React.Component{
+
+  //构造器
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // 渲染函数
+  render() {
+    return (
+        <View style={localStyle.flexCenter}>
+          <Text style={localStyle.textColorBlue}>关注</Text>
+        </View>
+    );
+  }
+
+  // 生命周期函数
+  //组件已挂载
+  componentDidMount() {
+
+  }
+
+  //组件将要卸载时
+  componentWillUnmount() {
+
+  }
+
+}
+
+
+export default FollowPage;
+
+// 样式
+const localStyle = {
+  textColorBlue: {
+    color: "blue"
+  },
+  flexCenter: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}
---
+++ src/pages/home/page/IndexPage.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {View, Text, ScrollView} from 'react-native';
+import ShowWorkBox from '../component/ShowWorkBox';
+import {request} from '../../../util/network/axiosRequest';
+import styles from '../../../assets/style/Styles';
+import ALPlaceView from '../../../components/al-place-view/ALPlaceView';
+
+class IndexPage extends React.Component{
+
+  //构造器
+  constructor(props) {
+    super(props);
+    this.state = {
+      navigation: props.navigation,
+      workList: [],
+    };
+  }
+
+  // 渲染函数
+  render() {
+    return (
+      <ScrollView>
+        <ALPlaceView height={20} />
+
+        {
+          this.state.workList.map((item, index) => {
+            return <ShowWorkBox key={item.title} data={item} />
+          })
+        }
+
+        <View style={{height: 50}}>
+          <Text style={[styles.alTextCenter, styles.alColorGray]}>
+            到底啦~
+          </Text>
+        </View>
+      </ScrollView>
+    );
+  }
+
+  // 生命周期函数
+  //组件已挂载
+  componentDidMount() {
+    this.getMockData();
+  }
+
+  //组件将要卸载时
+  componentWillUnmount() {
+
+  }
+
+  // 请求作品列表数据
+  getMockData = () => {
+    let url = React.mockPath + '/home_work_list.json';
+
+    request({
+      url: url,
+      method: 'GET',
+      data: {},
+    }).then(res => {
+      console.log(res.data.data);
+      this.setState({
+        workList: res.data.data
+      });
+      // console.log(this.state.workList);
+    }).catch(err => {
+      console.log(err);
+    });
+  };
+
+}
+
+
+export default IndexPage;
+
+// 样式
+const localStyle = {
+  textColorBlue: {
+    color: "blue"
+  },
+  flexCenter: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}

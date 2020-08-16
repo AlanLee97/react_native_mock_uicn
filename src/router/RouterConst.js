700e081a466467b5dd004f3ef138d278957c8317
1445654576@qq.com <1445654576@qq.com>
2020-08-16 13:01:09 +0800

    添加路由地址常量

--- src/router/RouterConst.js
+++ src/router/RouterConst.js
@@ -1,3 +1,12 @@
+//测试页
+export const TEST_PAGE = "TestPage";
+
+//登录页
+export const LOGIN_PAGE = "LoginPage";
+
+//注册页
+export const REGISTER_PAGE = "RegisterPage";
+
 //首页
 export const HOME_PAGE = "HomePage";
 
@@ -12,3 +21,9 @@ export const MESSAGE_PAGE = "MessagePage";
 
 //我的
 export const MY_PAGE = "MyPage";
+
+//作品详情页
+export const WORK_DETAIL_PAGE = "WorkDetailPage";
+
+
+
